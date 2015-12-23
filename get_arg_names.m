265f55372d7b5e41eb8012db2b4d3529077b93c6
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-23 01:40:59 -0600

    Use the system temp directory in get_arg_names
    This prevents conflicts where two files define the same function name
    and makes sure that the file does not stay in the current working
    directory.

--- get_arg_names.m
+++ get_arg_names.m
@@ -2,31 +2,34 @@
 % <http://stackoverflow.com/a/10746936>
 function [inputNames,outputNames] = get_arg_names(functionFile)
     %# get some random file name
+    tmp_folder = tempdir;
     fname = tempname;
     [~,fname] = fileparts(fname);
+    full_fname = fullfile( tmp_folder, [ fname '.m' ] );
 
     %# read input function content as string
     str = fileread(which(functionFile));
 
     %# build a class containing that function source, and write it to file
-    fid = fopen([fname '.m'], 'w');
+    fid = fopen(full_fname, 'w');
     fprintf(fid, 'classdef %s; methods;\n %s\n end; end', fname, str);
     fclose(fid);
 
     %# terminating function definition with an end statement is not
     %# always required, but now becomes required with classdef
     missingEndErrMsg = 'An END might be missing, possibly matching CLASSDEF.';
-    c = checkcode([fname '.m']);     %# run mlint code analyzer on file
+    c = checkcode(full_fname);     %# run mlint code analyzer on file
     if ismember(missingEndErrMsg,{c.message})
         % append "end" keyword to class file
-        str = fileread([fname '.m']);
-        fid = fopen([fname '.m'], 'w');
+        str = fileread(full_fname);
+        fid = fopen(full_fname, 'w');
         fprintf(fid, '%s \n end', str);
         fclose(fid);
     end
 
     %# refresh path to force MATLAB to detect new class
-    rehash
+    addpath(  tmp_folder );
+    rehash;
 
     %# introspection (deal with cases of nested/sub-function)
     m = meta.class.fromName(fname);
@@ -34,6 +37,9 @@ function [inputNames,outputNames] = get_arg_names(functionFile)
     inputNames = m.MethodList(idx).InputNames;
     outputNames = m.MethodList(idx).OutputNames;
 
+    rmpath(tmp_folder);
+    rehash;
+
     %# delete temp file when done
-    delete([fname '.m'])
+    delete(full_fname)
 end
