c694b54237718ed6c0d63dc26a7e8d2c84441063
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-23 18:48:14 -0600

    get all the caller input/output arguments depending on location
    This uses the debugger state information to know where the debugger has
    stopped. If it is at the beginning of a function, retrieve all the input
    arguments of the caller. If it is at the end of a function, retrieve the output
    arguments of the caller.

--- dump_state.m
+++ dump_state.m
@@ -1,16 +1,78 @@
-% This function can only be used when in debug mode.
-function dump_state()
+function caller_state = dump_state(path_to_output_directory)
+% function dump_state()
+%
+% Dumps the state of the current function.
+%
+% Note:
+%  - This function can only be used when in debug mode.
+
+	% where the output will be stored
+	caller_state = struct();
+	caller_state.TIME = cputime;
+
+	%% Get current function name
 	call_stack = dbstack('-completenames', 1);
 	assert( ~isempty( call_stack ),...
 		'Empty call stack. Called from outside debugger.');
+
+	% current_func is a struct with the fields
+	%  * .name : function name
+	%  * .file : full path to the file
+	%  * .line : line where stopped
+	current_func = call_stack(1);
+
+	% Get breakpoints for current file
+	current_file_bps = dbstatus( current_func.file );
+	bp_for_current_line = find(current_file_bps.line == current_func.line);
+	if numel( bp_for_current_line ) ~= 1
+		error( 'Found %d breakpoints for %s:%d when expecting 1',...
+				numel( break_point_for_current_line ),...
+				current_func.file,...
+				current_func.line );
+	end
 
 	%% Identify stopping location
 	% 1. function start
 	% 2. function end
 	% 3. I/O-related
 
+	% bp_expr_for_current_line is the expression for conditionally
+	% breaking at the given breakpoint
+	bp_expr_for_current_line = current_file_bps.expression{bp_for_current_line};
+	if regexp(bp_expr_for_current_line, '^tracer\(\s*''F'',\s*''begin''\s*\)$')
+		caller_state.WHERE = 'F_BEGIN';
+	elseif regexp(bp_expr_for_current_line, '^tracer\(\s*''F'',\s*''end''\s*\)$')
+		caller_state.WHERE = 'F_END';
+	end
+
 	%% Determine names of input and output variables.
-	[input_args, output_args] = get_arg_names(call_stack(1).name);
+	[input_args, output_args] = get_arg_names(current_func.name);
+
+	%% Store the input or output variables into the `caller_state`.
+	% NOTE: the loops below can not be moved to their own functions because
+	% that will add another function to the call stack and then
+	% evalin('caller', ...) will no longer work.
+	if strcmp( caller_state.WHERE, 'F_BEGIN' )
+		caller_state.input = struct();
+		for input_args_i = 1:length(input_args)
+			cur_input_arg = input_args{input_args_i};
+			caller_state.input = setfield(...
+				caller_state.input,...
+				cur_input_arg,...
+				evalin( 'caller', cur_input_arg )...
+			);
+		end
+	elseif strcmp( caller_state.WHERE, 'F_END' )
+		caller_state.output = struct();
+		for output_args_i = 1:length(output_args)
+			cur_output_arg = output_args{output_args_i};
+			caller_state.output = setfield(...
+				caller_state.output,...
+				cur_output_arg,...
+				evalin( 'caller', cur_output_arg )...
+			);
+		end
+	end
 
 	input_args
 	output_args
