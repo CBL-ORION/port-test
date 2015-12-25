5e55986b3abb27b6387bc58e6655ba3acc21e9a5
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-24 18:32:55 -0600

    remove false starts
    Neither Data::MATFile, PDL::IO:Matlab, nor PDL::IO:HDF5 work with the
    data in the .mat files that are being generated. This is likely because
    these contain structs and can not be loaded directly with the data
    models for each.

--- cpanfile
+++ cpanfile
@@ -1,4 +1,1 @@
 requires 'PDL';
-requires 'Data::MATFile';
-requires 'git@github.com:jlapeyre/PDL-IO-Matlab.git';
-requires 'PDL::IO::HDF5';
--- test-harness.pl
+++ test-harness.pl
@@ -4,19 +4,3 @@ use strict;
 use warnings;
 
 use PDL;
-use Data::MATFile qw/read_matfile/;;
-use PDL::IO::Matlab;
-use PDL::IO::HDF5;
-
-
-my $mat_hdf5 =  PDL::IO::HDF5->new('../orion/test.mat.v7.3');
-use DDP; p $mat_hdf5->attrGet("MATLAB_fields");
-
-
-#my @pdlsv7 =  matlab_read('../orion/test.mat.v7');
-#my @pdlsv73 =  matlab_read('../orion/test.mat.v7.3');
-#use DDP; p @pdlsv73;
-
-#my $matfilev7 = read_matfile ('../orion/test.mat.v7');
-#use DDP; p $matfilev7;
-#my $matfilev73 = read_matfile ('../orion/test.mat.v7.3');
