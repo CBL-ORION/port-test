33b2daa3f243760402d9c590523379901f1458f2
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-24 18:24:37 -0600

    try using PDL::IO::Matlab

--- test-harness.pl
+++ test-harness.pl
@@ -8,6 +8,10 @@ use Data::MATFile qw/read_matfile/;;
 use PDL::IO::Matlab;
 
 
+#my @pdlsv7 =  matlab_read('../orion/test.mat.v7');
+#my @pdlsv73 =  matlab_read('../orion/test.mat.v7.3');
+#use DDP; p @pdlsv73;
+
 #my $matfilev7 = read_matfile ('../orion/test.mat.v7');
 #use DDP; p $matfilev7;
 #my $matfilev73 = read_matfile ('../orion/test.mat.v7.3');
