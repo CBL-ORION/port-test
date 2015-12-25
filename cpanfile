694026607f78606d67a98153f971e4c4c0798d72
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-24 18:32:24 -0600

    try PDL::IO::HDF5

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
 requires 'PDL';
 requires 'Data::MATFile';
 requires 'git@github.com:jlapeyre/PDL-IO-Matlab.git';
+requires 'PDL::IO::HDF5';
--- test-harness.pl
+++ test-harness.pl
@@ -6,6 +6,11 @@ use warnings;
 use PDL;
 use Data::MATFile qw/read_matfile/;;
 use PDL::IO::Matlab;
+use PDL::IO::HDF5;
+
+
+my $mat_hdf5 =  PDL::IO::HDF5->new('../orion/test.mat.v7.3');
+use DDP; p $mat_hdf5->attrGet("MATLAB_fields");
 
 
 #my @pdlsv7 =  matlab_read('../orion/test.mat.v7');
