ba7dbb36c81af8e2ebec8db0e44007eedb8f5508
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-28 22:00:45 -0600

    test the ndarray3 typemap

--- test-harness.pl
+++ test-harness.pl
@@ -3,10 +3,37 @@
 use strict;
 use warnings;
 
+use v5.16;
 use lib 'lib';
 use PDL;
 use Data::MATLAB;
 
-my $p = Data::MATLAB->read_data( '../orion/test.mat.v7' );
+#use Inline Config =>
+	#enable => force_build =>
+	#enable => build_noisy =>
+	#disable => clean_after_build =>;
 
-use DDP; p $p;
+use FindBin;
+use Inline C => 'DATA',
+	ENABLE => AUTOWRAP =>
+	INC => "-I$FindBin::Bin/orion/lib",
+	LIBS => "-L$FindBin::Bin/orion/.build/.lib -lorion",
+	with => ['PDL'],
+	TYPEMAPS => 'typemap';
+
+#my $p = Data::MATLAB->read_data( '../orion/test.mat.v7' );
+#use DDP; p $p;
+
+my $q = orion_hdaf(3, 5, sequence(float, 3,3,3));
+use DDP; p $q;
+
+__DATA__
+__C__
+
+#include "ndarray/ndarray3.h"
+/*#include "kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/IsotropicFilter/hdaf.h"*/
+
+extern ndarray3* orion_hdaf(
+		int hdaf_approx_degree,
+		float scaling_constant,
+		ndarray3* x);
