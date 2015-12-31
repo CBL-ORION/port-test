9f259c13a3e55b5c57a343b2a301f2263e505a9c
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-31 05:53:39 -0600

    wip: parse all the matlab functions

---
+++ src/matlab/parse_project_matlab_funcs.m
@@ -0,0 +1,42 @@
+function parse_project_matlab_funcs(path_to_project, target_file)
+addpath(genpath(path_to_project));
+
+m_funcs = get_project_matlab_funcs(path_to_project);
+
+%m_funcs
+
+func_data = struct();
+
+func_data.name = {};
+func_data.input_param = {};
+func_data.output_param = {};
+
+skipped = {};
+
+for m_funcs_i = 1:size( m_funcs, 1 )
+	current_func = struct();
+
+	current_func.name = m_funcs{ m_funcs_i, 1 };
+
+	disp( sprintf('Processing function %s...',current_func.name) );
+
+	try
+		[input_param, output_param] = get_arg_names(current_func.name);
+	catch
+		skipped = [ skipped, current_func.name ];
+		continue
+	end
+
+	current_func.input_param = input_param;
+	current_func.output_param = output_param;
+
+	func_data = [ func_data, current_func ];
+end
+
+data = struct();
+data.functions = func_data;
+data.skipped_functions = skipped;
+
+save( target_file, 'data', '-v7' );
+
+end
