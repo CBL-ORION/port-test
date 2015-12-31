13fadfb896f972956f4541b920ce73bcf7f742f8
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-31 05:53:23 -0600

    refactor the MATLAB function list code

---
+++ src/matlab/get_project_matlab_funcs.m
@@ -0,0 +1,19 @@
+function [m_funcs] = get_project_matlab_funcs(path_to_project)
+
+%% Get a list of the all MATLAB functions.
+% This iterates over each of the subdirectories of the path and gets
+% the .m files therein.
+m_files = {};
+all_dir = strsplit(genpath(path_to_project), pathsep);
+for dir_i = 1:length(all_dir)
+	m_files_in_dir = what(all_dir{dir_i});
+	m_files = [m_files; m_files_in_dir.m];
+end
+
+m_funcs = cell(length(m_files));
+for m_files_i = 1:length( m_files )
+	m_funcs{m_files_i} = regexprep(m_files{m_files_i} , '\.m$' , '');
+	%disp( [m_files{m_files_i} ' -> ' m_funcs{m_files_i}]  );%DEBUG
+end
+
+end
--- src/matlab/otrace.m
+++ src/matlab/otrace.m
@@ -1,23 +1,10 @@
 function otrace(path_to_project, code_to_eval)
 	%% Add the top level of the path to the MATLAB path.
 	addpath(genpath(path_to_project));
-	addpath(genpath('tracer4m'));
+	[path_to_current_mat,~,~] = fileparts(mfilename('fullpath'));
+	addpath(genpath(path_to_current_mat));
 
-	%% Get a list of the all MATLAB functions.
-	% This iterates over each of the subdirectories of the path and gets
-	% the .m files therein.
-	m_files = {};
-	all_dir = strsplit(genpath(path_to_project), pathsep);
-	for dir_i = 1:length(all_dir)
-		m_files_in_dir = what(all_dir{dir_i});
-		m_files = [m_files; m_files_in_dir.m];
-	end
-
-	m_funcs = cell(size(m_files));
-	for m_files_i = 1:length( m_files )
-		m_funcs{m_files_i} = regexprep(m_files{m_files_i} , '\.m$' , '');
-		%disp( [m_files{m_files_i} ' -> ' m_funcs{m_files_i}]  );%DEBUG
-	end
+	m_funcs = get_project_matlab_funcs(path_to_project);
 
 	%% Set up tracer for debugging
 	log = TraceHistory.Instance;
