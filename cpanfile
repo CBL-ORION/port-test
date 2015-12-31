e7cadb241c2c151a4e1022cce4d728e13d091891
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-31 04:57:14 -0600

    use a C grammar to parse function declaration
    Taken and modified from Inline::C.

--- cpanfile
+++ cpanfile
@@ -3,3 +3,6 @@ requires 'Inline::C', '0.64';
 requires 'Path::Tiny';
 requires 'Hash::Merge';
 requires 'Test::Most';
+requires 'Type::Tiny';
+requires 'Path::Iterator::Rule';
+requires 'Parse::RecDescent';
--- lib/ORION.pm
+++ lib/ORION.pm
@@ -6,6 +6,8 @@ use warnings;
 use PDL;
 use Path::Tiny;
 use Hash::Merge;
+use Path::Iterator::Rule;
+use Parse::RecDescent;
 
 sub basedir {
 	my $file = path(__FILE__)->absolute;
@@ -17,6 +19,11 @@ sub basedir {
 sub oriondir {
 	my $base_dir = basedir();
 	$base_dir->child( qw{ orion });
+}
+
+sub orionmatdir {
+	my $base_dir = basedir();
+	$base_dir->child( qw{ external orionmat });
 }
 
 sub Inline {
@@ -40,5 +47,147 @@ sub Inline {
 	return $merged_config;
 }
 
+sub c_functions {
+	# get all the .h files under lib/
+	my $header_file_rule = Path::Iterator::Rule->new
+		->file->name( qr/\.h$/ );
+	my $header_file_iter = $header_file_rule->iter( ORION->oriondir->child('lib') );
+	while( defined( my $file = $header_file_iter->() ) ) {
+		my $code = path($file)->slurp_utf8;
+		my $parser = Parse::RecDescent->new(c_grammar());
+		$parser->{data}{AUTOWRAP} = 1;
+		$parser->{data}{file} = $file;
+		$parser->code( $code ) or die "could not parse file $file";
+		use DDP; p $parser->{data};
+		#my $cAstObject = MarpaX::Languages::C::AST->new( lazy => 1 );
+		#my $r = $cAstObject->parse(\$code);
+	}
+
+}
+
+sub matlab_functions {
+	# get all the .h files under lib/
+	my $m_file_rule = Path::Iterator::Rule->new
+		->file->name( qr/\.m$/ );
+	my $m_file_iter = $m_file_rule->iter( ORION->orionmatdir );
+	while( defined( my $file = $m_file_iter->() ) ) {
+		print "$file\n";
+	}
+}
+
+sub c_grammar {
+    <<'END';
+
+code:   part(s)
+        {
+         return 1;
+        }
+
+part:   comment
+      | function_definition
+        {
+         my $function = $item[1][0];
+         $return = 1, last if $thisparser->{data}{done}{$function}++;
+         push @{$thisparser->{data}{functions}}, $function;
+         $thisparser->{data}{function}{$function}{return_type} =
+             $item[1][1];
+         $thisparser->{data}{function}{$function}{arg_types} =
+             [map {ref $_ ? $_->[0] : '...'} @{$item[1][2]}];
+         $thisparser->{data}{function}{$function}{arg_names} =
+             [map {ref $_ ? $_->[1] : '...'} @{$item[1][2]}];
+        }
+      | function_declaration
+        {
+         $return = 1, last unless $thisparser->{data}{AUTOWRAP};
+         my $function = $item[1][0];
+         $return = 1, last if $thisparser->{data}{done}{$function}++;
+         my $dummy = 'arg1';
+         push @{$thisparser->{data}{functions}}, $function;
+         $thisparser->{data}{function}{$function}{return_type} =
+             $item[1][1];
+         $thisparser->{data}{function}{$function}{arg_types} =
+             [map {ref $_ ? $_->[0] : '...'} @{$item[1][2]}];
+         $thisparser->{data}{function}{$function}{arg_names} =
+             [map {ref $_ ? ($_->[1] || $dummy++) : '...'} @{$item[1][2]}];
+        }
+      | anything_else
+
+comment:
+        m{\s* // [^\n]* \n }x
+      | m{\s* /\* (?:[^*]+|\*(?!/))* \*/  ([ \t]*)? }x
+
+function_definition:
+        rtype IDENTIFIER '(' <leftop: arg ',' arg>(s?) ')' '{'
+        {
+         [@item[2,1], $item[4]]
+        }
+
+function_declaration:
+        rtype IDENTIFIER '(' <leftop: arg_decl ',' arg_decl>(s?) ')' ';'
+        {
+         [@item[2,1], $item[4]]
+        }
+
+rtype:  rtype1 | rtype2
+
+rtype1: modifier(s?) TYPE star(s?)
+        {
+         $return = $item[2];
+         $return = join ' ',@{$item[1]},$return
+           if @{$item[1]} and $item[1][0] ne 'extern';
+         $return .= join '',' ',@{$item[3]} if @{$item[3]};
+         #return undef unless (defined $thisparser->{data}{typeconv}
+                                                   #{valid_rtypes}{$return});
+        }
+
+rtype2: modifier(s) star(s?)
+        {
+         $return = join ' ',@{$item[1]};
+         $return .= join '',' ',@{$item[2]} if @{$item[2]};
+         #return undef unless (defined $thisparser->{data}{typeconv}
+                                                   #{valid_rtypes}{$return});
+        }
+
+arg:    type IDENTIFIER {[@item[1,2]]}
+      | '...'
+
+arg_decl:
+        type IDENTIFIER(s?) {[$item[1], $item[2][0] || '']}
+      | '...'
+
+type:   type1 | type2
+
+type1:  modifier(s?) TYPE star(s?)
+        {
+         $return = $item[2];
+         $return = join ' ',@{$item[1]},$return if @{$item[1]};
+         $return .= join '',' ',@{$item[3]} if @{$item[3]};
+         #return undef unless (defined $thisparser->{data}{typeconv}
+                                                   #{valid_types}{$return});
+        }
+
+type2:  modifier(s) star(s?)
+        {
+         $return = join ' ',@{$item[1]};
+         $return .= join '',' ',@{$item[2]} if @{$item[2]};
+         #return undef unless (defined $thisparser->{data}{typeconv}
+                                                   #{valid_types}{$return});
+        }
+
+modifier:
+        'unsigned' | 'long' | 'extern' | 'const'
+
+star:   '*'
+
+IDENTIFIER:
+        /\w+/
+
+TYPE:   /\w+/
+
+anything_else:
+        /.*/
+
+END
+}
 
 1;
