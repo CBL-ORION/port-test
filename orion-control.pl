5b5049edb17ab7b00c6d43308cec49b0d1492335
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-31 18:16:40 -0600

    set up the automated MATLAB tracing with ORION^m

--- orion-control.pl
+++ orion-control.pl
@@ -8,13 +8,24 @@ use Expect;
 my $command = Expect->spawn("matlab", qw(-nosplash -nodesktop -nojvm))
 	or die "Couldn't start program: $!\n";
 
-say $command "otrace('test')";
+
+my $matlab_source = ORION->matlabsrcdir;
+my $project = ORION->orionmatdir;
+my $debug_trace_dir = ORION->datadir->child(qw('debug-trace'));
+my $orion3mat_test_data_conf = ORION->oriondir->child(qw(test-data DIADEM NPF Input_NPF023_D.txt));
+$debug_trace->mkpath;
+my $SETUP_EXEC = join ",", (
+	"addpath('$matlab_source')",
+	"otrace('$project', 'ORION3(''$orion3mat_test_data_conf'')')",
+);
+
+say $command $SETUP_EXEC;
 
 $command->debug( 2 );
 
 my $it = 0;
 while ( $command->expect(undef, '>>' ) ) {
-	say $command "dump_state, dbcont";
+	say $command "dump_state('$debug_trace_dir'), dbcont";
 	$it++;
 	last if $it > 5;
 }
