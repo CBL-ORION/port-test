69b3be2df3d5b9cab262ccb990094edcba4791f7
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-03-24 18:32:41 -0500

    Add method to check if C type is a pointer

--- lib/ORION/C/Type.pm
+++ lib/ORION/C/Type.pm
@@ -6,11 +6,16 @@ use Moo;
 
 has decl => ( is => 'ro' );
 
-has [ qw(is_const unqualified_type) ] =>  ( is => 'lazy' );
+has [ qw(is_const is_ptr unqualified_type) ] =>  ( is => 'lazy' );
 
 sub _build_is_const {
 	my ($self) = @_;
 	return $self->decl =~ /^const\b/;
+}
+
+sub _build_is_ptr {
+	my ($self) = @_;
+	return $self->unqualified_type =~ /\*$/;
 }
 
 sub _build_unqualified_type {
