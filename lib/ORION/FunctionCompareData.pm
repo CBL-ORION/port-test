5805e6964845e11b627de959278221b621d87ab9
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-03-24 18:34:39 -0500

    Add parameter mapping for a few more functions

--- lib/ORION/FunctionCompareData.pm
+++ lib/ORION/FunctionCompareData.pm
@@ -39,7 +39,35 @@ our $MAPPING = {
 			p => 'param',
 		},
 	},
+	computeFeatures => {
+		inject => sub {
+			my ($fs) = @_;
+			my $map = {};
+			my $vol_map = file_paths_map_to_vol($fs);
+			$map = merge( $map, $vol_map );
+
+			# sigma,apply_log,file_name_scales
+			my $param_data = {
+				sigma => $fs->input->{sigma},
+				apply_log => $fs->input->{apply_log},
+			};
+			my $param = ORION::Types->coerce_type(
+				ORION::C::Type->new( decl => 'orion_segmentation_param *' ),
+				$param_data,
+			);
+
+			$map = merge( $map, { param => $param } );
+
+			return $map;
+		}
+	},
 	getFeatures => {
+		param_map => {
+			p => 'param',
+		},
+		inject => \&file_paths_map_to_vol,
+	},
+	readNegativeSamples => {
 		param_map => {
 			p => 'param',
 		},
@@ -52,6 +80,27 @@ our $MAPPING = {
 		inject => sub {
 			my $p = file_paths_map_to_vol(@_);
 			$p->{input_volume} = delete $p->{vol};
+			$p;
+		},
+	},
+	computeEigenvaluesGaussianFilter => {
+		inject => sub {
+			my $p = file_paths_map_to_vol(@_);
+			$p->{input_volume} = delete $p->{vol};
+
+			my $method;
+			my $method_enum;
+			$method_enum->{EIG_FEAT_METHOD_SORT_SATO} = 1;
+			$method_enum->{EIG_FEAT_METHOD_SORT_FRANGI} = 2;
+			$method_enum->{EIG_FEAT_METHOD_SINGLE_VALUE} = 3;
+			given( $p->{sorting} ) {
+				when('FRANGI')          { $method = $method_enum->{EIG_FEAT_METHOD_SORT_FRANGI} }
+				when('SATO')            { $method = $method_enum->{EIG_FEAT_METHOD_SORT_SATO} }
+				when('ORION1_Features') { $method = $method_enum->{EIG_FEAT_METHOD_SINGLE_VALUE} }
+			}
+
+			$p->{method} = $method;
+
 			$p;
 		},
 	},
